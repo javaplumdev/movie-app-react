3361afab42ccbea657138bd657de1f294b3f9fb1
Charlito Mecarez Jr <70713746+javaplumdev@users.noreply.github.com>
2022-04-23 20:40:23 +0800

    update

--- movie-app/src/components/LandingPage.jsx
+++ movie-app/src/components/LandingPage.jsx
@@ -3,19 +3,19 @@ import axios from 'axios';
 
 function LandingPage() {
 	const API_KEY = `3774131603660110c024a22c82fb41fe`;
-	const apiLink = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1&include_video=true`;
 
-	const [popularMovies, setPopularMovies] = useState(null);
+	const popular_movies_url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1&include_video=true`;
+	const [postPopularMovies, setPostPopularMovies] = useState(null);
 
 	useEffect(() => {
-		axios.get(apiLink).then((response) => {
-			setPopularMovies(response.data);
+		axios.get(popular_movies_url).then((response) => {
+			setPostPopularMovies(response.data);
 		});
 	}, []);
 
 	return (
 		<>
-			{popularMovies.results.map((movie) => {
+			{postPopularMovies.results.map((movie) => {
 				return <p key={movie.id}>{movie.original_title}</p>;
 			})}
 		</>
